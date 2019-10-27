ed4cafbd90845cf64c9dd96560c6e50f19a207b9
Michael Zarechiy <mjee245@gmail.com>
2019-10-27 09:15:44 +0200

    Change staticfiles path

--- tickets/settings.py
+++ tickets/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/var/www/help.anserglob.ua/static/"
 
 
 MEDIA_ROOT = os.path.join('/home/mjee/media')
