7a3e87ae217d6520b760f7b5370b0d8685a436e9
Михаил Заречный <mjee245@gmail.com>
2019-12-10 15:56:30 +0200

    1.Добавил в модель "Конаткты" свойства полям "Уникальные". 2. Отключил наследование шаблонов у тикетов.

--- contacts/models.py
+++ contacts/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 
 class Company(models.Model):
-    company = models.CharField(max_length=25)
+    company = models.CharField(max_length=25, unique=True)
 
     def __str__(self):
         return str(self.company)
 
 
 class Position(models.Model):
-    position = models.CharField(max_length=75)
+    position = models.CharField(max_length=75, unique=True)
 
     def __str__(self):
         return str(self.position)
 
 
 class Mail(models.Model):
-    mail = models.CharField(max_length=40)
+    mail = models.CharField(max_length=40, unique=True)
 
     def __str__(self):
         return str(self.mail)
@@ -31,14 +31,14 @@ class Tel(models.Model):
 
 
 class Department(models.Model):
-    department = models.CharField(max_length=55)
+    department = models.CharField(max_length=55, unique=True)
 
     def __str__(self):
         return str(self.department)
 
 
 class Location(models.Model):
-    location = models.CharField(max_length=55)
+    location = models.CharField(max_length=55, unique=True)
 
     def __str__(self):
         return str(self.location)
@@ -54,7 +54,7 @@ class Contacts(models.Model):
     tel_id = models.ForeignKey(Tel, null=True, blank=True)
     mail_id = models.ForeignKey(Mail, null=True, blank=True)
     info = models.TextField(max_length=250, blank=True)
-    is_shief = models.BooleanField(default=False)
+    is_chief = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.family_name)
